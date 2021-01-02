09df8eef5816846f6898c12cad2ba81428e708e5
Amari Duran <jch.duran1@gmail.com>
2021-01-02 04:50:40 -0600

    Initial Commit

---
+++ Discover/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Discover
+//
+//  Created by Cleopatra on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Discover/DiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverApp.swift
+//  Discover
+//
+//  Created by Cleopatra on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
